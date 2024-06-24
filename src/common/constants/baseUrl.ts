2688361c1cf20efd5755c4db702abd5a861afe16
Nataly Baranova <borashek@inbox.ru>
2024-06-24 15:45:07 +0300

    feat: https

--- src/common/constants/baseUrl.ts
+++ src/common/constants/baseUrl.ts
@@ -1,3 +1,3 @@
 import { eID } from "./eID";
 
-export const baseUrl = `http://185.244.172.108:8081/v1/outlay-rows/entity/${eID.id}`
+export const baseUrl = `https://185.244.172.108:8081/v1/outlay-rows/entity/${eID.id}`
